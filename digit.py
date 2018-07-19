3ac10d19ebd9b5377e68d110456346ee5f3c09c3
wuling31715 <wuling31715@gmail.com>
2018-07-19 11:19:04 +0800

    digit

--- digit.py
+++ digit.py
@@ -14,7 +14,7 @@ def main(base_image_path, style_reference_image_path, result_prefix, iterations)
     # parser.add_argument('style_reference_image_path', metavar='ref', type=str, help='Path to the style reference image.')
     # parser.add_argument('result_prefix', metavar='res_prefix', type=str, help='Prefix for the saved results.')
     # parser.add_argument('iter', type=int, default=10, help='Number of iterations to run.')
-    # parser.add_argument('--content_weight', type=float, default=0.025, required=False, help='Content weight.')
+    parser.add_argument('--content_weight', type=float, default=0.025, required=False, help='Content weight.')
     parser.add_argument('--style_weight', type=float, default=1.0, required=False, help='Style weight.')
     parser.add_argument('--tv_weight', type=float, default=1.0, required=False, help='Total Variation weight.')
 
