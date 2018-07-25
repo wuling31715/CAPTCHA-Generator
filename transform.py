0b9d8073d95985825451886ade7ec37207fcef03
wuling31715 <wuling31715@gmail.com>
2018-07-25 14:46:30 +0800

    result5

--- transform.py
+++ transform.py
@@ -230,4 +230,4 @@ def main(base_image_path, style_reference_image_path, result_path, result_prefix
 
 begin_time = time.time()
 for i in range(10):
-    main(('digit/%d.png' % i), 'style/halftone.png', 'result4/%d/' % (i), 'result4/%d/0%d0' % (i, i), 100, 0.0001, 0.9)
+    main(('digit/%d.png' % i), 'style/halftone.png', 'result5/%d/' % (i), 'result5/%d/0%d0' % (i, i), 10, 0.0001, 2.0)
