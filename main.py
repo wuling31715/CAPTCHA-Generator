84b33708cdf15c72454350e371dd4aa6f73cd515
wuling31715 <wuling31715@gmail.com>
2018-07-31 09:47:45 +0800

    a

--- main.py
+++ main.py
@@ -237,4 +237,4 @@ for i in os.listdir(path):
 
 begin_time = time.time()
 for i in file_list:
-    main(('test/%s' % i), 'style/halftone_400.png', ('test/%s/' % i), 'test/%s/%s' % (i, i), 100, 0.025, 1.0)
+    main(('test/%s.png' % i), 'style/halftone_400.png', ('test/%s/' % i), 'test/%s/%s' % (i, i), 100, 0.025, 1.0)
