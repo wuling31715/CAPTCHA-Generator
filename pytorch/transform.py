81cdfb61d4beed0cda92d28cb70f1912694ae8a1
wuling31715 <wuling31715@gmail.com>
2018-08-02 21:53:03 +0800

    test3

--- pytorch/transform.py
+++ pytorch/transform.py
@@ -153,7 +153,7 @@ def run_style_transfer(cnn, normalization_mean, normalization_std, content_img, 
             run[0] += 1
             if run[0] % 100 == 0:
                 print(path)
-                img_name  = path + 'weight%diteration%d.png' % (style_weight, run[0])
+                img_name = path + 'weight%diteration%d.png' % (style_weight, run[0])
                 imsave(input_img, img_name)
                 print("Run {}:".format(run))
                 print('Style Loss : {:4f} Content Loss: {:4f}'.format(style_score.item(), content_score.item()))
@@ -168,7 +168,7 @@ cnn = models.vgg19(pretrained=True).features.to(device).eval()
 cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
-style_img = image_loader("images/picasso.jpg")
-content_img = image_loader("images/dancing.jpg")
+style_img = image_loader("images/halftone_256.jpg")
+content_img = image_loader("images/1.jpg")
 input_img = content_img.clone()
-output = run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std, content_img, style_img, input_img, 10000, 1000000, 'test2/')
+output = run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std, content_img, style_img, input_img, 10000, 1000000, 'test3/')
