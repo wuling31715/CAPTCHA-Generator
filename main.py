66f1cf2857f6b3e175aabc4c80b68a3302d8ff92
wuling31715 <wuling31715@gmail.com>
2018-08-03 18:04:51 +0800

    test2

--- main.py
+++ main.py
@@ -235,6 +235,6 @@ for i in os.listdir(path):
 
 begin_time = time.time()
 for i in file_list:
-    j = 1
-    main((path + i), 'style/halftone_28.png', ('mnist/result/%s/W%s/' % (str(i), str(j))), 1, 1.0, j)
-    print('%s done' % str(i))
+    for j in range(10):
+        j = 10 ** j
+        main((path + i), 'style/halftone_28.png', ('mnist/result/%s/W%s/' % (str(i), str(j))), 20, 1.0, j)
