21037df5e3eff14faf846478fa567ef07aeb8c96
wuling31715 <wuling31715@gmail.com>
2018-08-03 15:42:26 +0800

    a

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ def main(base_image_path, style_reference_image_path, result_path, iterations, c
         print('Total Running Time: %ds' % (end_time - begin_time))
         print()
 
-path = 'digit'
+path = 'mnist/'
 file_list = list()
 for i in os.listdir(path):
     if '.png' in i:
@@ -235,7 +235,7 @@ for i in os.listdir(path):
         file_list.append(j)
 
 begin_time = time.time()
-for i in range(10):
-    j = 10 ** i
-    main(('digit/%s.png' % str(1)), 'style/halftone_256.png', ('test/256/W%s/' % str(j)), 20, 1.0, j)    
-    main(('digit/%s.png' % str(1)), 'style/halftone_512.png', ('test/512/W%s/' % str(j)), 20, 1.0, j)
+for i in file_list:
+    for j in range(10):
+        j = 10 ** j
+        main(('digit/%s.png' % str(i)), 'style/halftone_28.png', ('mnist/result/W%s/' % str(j)), 20, 1.0, j)
