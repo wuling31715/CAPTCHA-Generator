162b04c547333c2f72888f98d6f31c391bcf52d8
wuling31715 <wuling31715@gmail.com>
2018-08-03 10:42:59 +0800

    model1

--- pytorch/WCT.py
+++ pytorch/WCT.py
@@ -27,7 +27,7 @@ parser.add_argument('--decoder2', default='models/feature_invertor_conv2_1.t7', 
 parser.add_argument('--decoder1', default='models/feature_invertor_conv1_1.t7', help='Path to the decoder1')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--batch_size', type=int, default=1, help='batch size')
-parser.add_argument('--fineSize', type=int, default=512, help='resize image to fineSize x fineSize,leave it to 0 if not resize')
+parser.add_argument('--fineSize', type=int, default=256, help='resize image to fineSize x fineSize,leave it to 0 if not resize')
 parser.add_argument('--outf', default='samples/', help='folder to output images')
 parser.add_argument('--alpha', type=float,default=1, help='hyperparameter to blend wct feature and content feature')
 parser.add_argument('--gpu', type=int, default=0, help="which gpu to run on.  default is 0")
@@ -41,40 +41,38 @@ except OSError:
 
 # Data loading code
 dataset = Dataset(args.contentPath,args.stylePath,args.fineSize)
-loader = torch.utils.data.DataLoader(dataset=dataset,
-                                     batch_size=1,
-                                     shuffle=False)
+loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=1, shuffle=False)
 
 wct = WCT(args)
-def styleTransfer(contentImg,styleImg,imname,csF):
+def styleTransfer(contentImg, styleImg, imname, csF):
 
-    sF5 = wct.e5(styleImg)
-    cF5 = wct.e5(contentImg)
-    sF5 = sF5.data.cpu().squeeze(0)
-    cF5 = cF5.data.cpu().squeeze(0)
-    csF5 = wct.transform(cF5,sF5,csF,args.alpha)
-    Im5 = wct.d5(csF5)
+    # sF5 = wct.e5(styleImg)
+    # cF5 = wct.e5(contentImg)
+    # sF5 = sF5.data.cpu().squeeze(0)
+    # cF5 = cF5.data.cpu().squeeze(0)
+    # csF5 = wct.transform(cF5,sF5,csF,args.alpha)
+    # Im5 = wct.d5(csF5)
 
-    sF4 = wct.e4(styleImg)
-    cF4 = wct.e4(Im5)
-    sF4 = sF4.data.cpu().squeeze(0)
-    cF4 = cF4.data.cpu().squeeze(0)
-    csF4 = wct.transform(cF4,sF4,csF,args.alpha)
-    Im4 = wct.d4(csF4)
+    # sF4 = wct.e4(styleImg)
+    # cF4 = wct.e4(Im5)
+    # sF4 = sF4.data.cpu().squeeze(0)
+    # cF4 = cF4.data.cpu().squeeze(0)
+    # csF4 = wct.transform(cF4,sF4,csF,args.alpha)
+    # Im4 = wct.d4(csF4)
 
-    sF3 = wct.e3(styleImg)
-    cF3 = wct.e3(Im4)
-    sF3 = sF3.data.cpu().squeeze(0)
-    cF3 = cF3.data.cpu().squeeze(0)
-    csF3 = wct.transform(cF3,sF3,csF,args.alpha)
-    Im3 = wct.d3(csF3)
+    # sF3 = wct.e3(styleImg)
+    # cF3 = wct.e3(Im4)
+    # sF3 = sF3.data.cpu().squeeze(0)
+    # cF3 = cF3.data.cpu().squeeze(0)
+    # csF3 = wct.transform(cF3,sF3,csF,args.alpha)
+    # Im3 = wct.d3(csF3)
 
-    sF2 = wct.e2(styleImg)
-    cF2 = wct.e2(Im3)
-    sF2 = sF2.data.cpu().squeeze(0)
-    cF2 = cF2.data.cpu().squeeze(0)
-    csF2 = wct.transform(cF2,sF2,csF,args.alpha)
-    Im2 = wct.d2(csF2)
+    # sF2 = wct.e2(styleImg)
+    # cF2 = wct.e2(Im3)
+    # sF2 = sF2.data.cpu().squeeze(0)
+    # cF2 = cF2.data.cpu().squeeze(0)
+    # csF2 = wct.transform(cF2,sF2,csF,args.alpha)
+    # Im2 = wct.d2(csF2)
 
     sF1 = wct.e1(styleImg)
     cF1 = wct.e1(Im2)
@@ -82,8 +80,7 @@ def styleTransfer(contentImg,styleImg,imname,csF):
     cF1 = cF1.data.cpu().squeeze(0)
     csF1 = wct.transform(cF1,sF1,csF,args.alpha)
     Im1 = wct.d1(csF1)
-    # save_image has this wired design to pad images with 4 pixels at default.
-    vutils.save_image(Im1.data.cpu().float(),os.path.join(args.outf,imname))
+    vutils.save_image(Im1.data.cpu().float(), os.path.join(args.outf, imname))
     return
 
 avgTime = 0
@@ -96,7 +93,7 @@ if(args.cuda):
     sImg = sImg.cuda(args.gpu)
     csF = csF.cuda(args.gpu)
     wct.cuda(args.gpu)
-for i,(contentImg,styleImg,imname) in enumerate(loader):
+for i,(contentImg, styleImg, imname) in enumerate(loader):
     imname = imname[0]
     print('Transferring ' + imname)
     if (args.cuda):
@@ -106,9 +103,9 @@ for i,(contentImg,styleImg,imname) in enumerate(loader):
     sImg = Variable(styleImg, volatile=True)
     start_time = time.time()
     # WCT Style Transfer
-    styleTransfer(cImg,sImg,imname,csF)
+    styleTransfer(cImg, sImg, imname,csF)
     end_time = time.time()
     print('Elapsed time is: %f' % (end_time - start_time))
     avgTime += (end_time - start_time)
 
-print('Processed %d images. Averaged time is %f' % ((i+1),avgTime/(i+1)))
+print('Processed %d images. Averaged time is %f' % ((i+1), avgTime/(i+1)))
