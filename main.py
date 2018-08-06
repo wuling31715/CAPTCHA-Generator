1cfe5747312c90818ed24b3888e4b83906792710
wuling31715 <wuling31715@gmail.com>
2018-08-06 17:02:30 +0800

    main.py

--- main.py
+++ main.py
@@ -76,149 +76,157 @@ def main(base_image_path_dir, style_reference_image_path, result_prefix_dir, ite
     # get the symbolic outputs of each "key" layer (we gave them unique names).
     outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
 
-    print(input_tensor)
-
-
-
-    # compute the neural style loss
-    # first we need to define 4 util functions
-
-    # the gram matrix of an image tensor (feature-wise outer product)
-
-
-    def gram_matrix(x):
-        assert K.ndim(x) == 3
+    for i in range(60000):
+        img_index = i
+        base_image_path = base_image_path_dir + str(img_index) + '.png'
+        
         if K.image_data_format() == 'channels_first':
-            features = K.batch_flatten(x)
+            combination_image = K.placeholder((1, 3, img_nrows, img_ncols))
         else:
-            features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
-        gram = K.dot(features, K.transpose(features))
-        return gram
-
-    # the "style loss" is designed to maintain
-    # the style of the reference image in the generated image.
-    # It is based on the gram matrices (which capture style) of
-    # feature maps from the style reference image
-    # and from the generated image
+            combination_image = K.placeholder((1, img_nrows, img_ncols, 3))
 
 
-    def style_loss(style, combination):
-        assert K.ndim(style) == 3
-        assert K.ndim(combination) == 3
-        S = gram_matrix(style)
-        C = gram_matrix(combination)
-        channels = 3
-        size = img_nrows * img_ncols
-        return K.sum(K.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
-
-    # an auxiliary loss function
-    # designed to maintain the "content" of the
-    # base image in the generated image
 
 
-    def content_loss(base, combination):
-        return K.sum(K.square(combination - base))
+        # compute the neural style loss
+        # first we need to define 4 util functions
 
-    # the 3rd loss function, total variation loss,
-    # designed to keep the generated image locally coherent
+        # the gram matrix of an image tensor (feature-wise outer product)
 
 
-    def total_variation_loss(x):
-        assert K.ndim(x) == 4
-        if K.image_data_format() == 'channels_first':
-            a = K.square(x[:, :, :img_nrows - 1, :img_ncols - 1] - x[:, :, 1:, :img_ncols - 1])
-            b = K.square(x[:, :, :img_nrows - 1, :img_ncols - 1] - x[:, :, :img_nrows - 1, 1:])
-        else:
-            a = K.square(x[:, :img_nrows - 1, :img_ncols - 1, :] - x[:, 1:, :img_ncols - 1, :])
-            b = K.square(x[:, :img_nrows - 1, :img_ncols - 1, :] - x[:, :img_nrows - 1, 1:, :])
-        return K.sum(K.pow(a + b, 1.25))
+        def gram_matrix(x):
+            assert K.ndim(x) == 3
+            if K.image_data_format() == 'channels_first':
+                features = K.batch_flatten(x)
+            else:
+                features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
+            gram = K.dot(features, K.transpose(features))
+            return gram
 
-    # combine these loss functions into a single scalar
-    loss = K.variable(0.)
-    layer_features = outputs_dict['block5_conv2']
-    base_image_features = layer_features[0, :, :, :]
-    combination_features = layer_features[2, :, :, :]
-    loss += content_weight * content_loss(base_image_features, combination_features)
+        # the "style loss" is designed to maintain
+        # the style of the reference image in the generated image.
+        # It is based on the gram matrices (which capture style) of
+        # feature maps from the style reference image
+        # and from the generated image
 
-    feature_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
-    for layer_name in feature_layers:
-        layer_features = outputs_dict[layer_name]
-        style_reference_features = layer_features[1, :, :, :]
+
+        def style_loss(style, combination):
+            assert K.ndim(style) == 3
+            assert K.ndim(combination) == 3
+            S = gram_matrix(style)
+            C = gram_matrix(combination)
+            channels = 3
+            size = img_nrows * img_ncols
+            return K.sum(K.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
+
+        # an auxiliary loss function
+        # designed to maintain the "content" of the
+        # base image in the generated image
+
+
+        def content_loss(base, combination):
+            return K.sum(K.square(combination - base))
+
+        # the 3rd loss function, total variation loss,
+        # designed to keep the generated image locally coherent
+
+
+        def total_variation_loss(x):
+            assert K.ndim(x) == 4
+            if K.image_data_format() == 'channels_first':
+                a = K.square(x[:, :, :img_nrows - 1, :img_ncols - 1] - x[:, :, 1:, :img_ncols - 1])
+                b = K.square(x[:, :, :img_nrows - 1, :img_ncols - 1] - x[:, :, :img_nrows - 1, 1:])
+            else:
+                a = K.square(x[:, :img_nrows - 1, :img_ncols - 1, :] - x[:, 1:, :img_ncols - 1, :])
+                b = K.square(x[:, :img_nrows - 1, :img_ncols - 1, :] - x[:, :img_nrows - 1, 1:, :])
+            return K.sum(K.pow(a + b, 1.25))
+
+        # combine these loss functions into a single scalar
+        loss = K.variable(0.)
+        layer_features = outputs_dict['block5_conv2']
+        base_image_features = layer_features[0, :, :, :]
         combination_features = layer_features[2, :, :, :]
-        sl = style_loss(style_reference_features, combination_features)
-        loss += (style_weight / len(feature_layers)) * sl
-    loss += total_variation_weight * total_variation_loss(combination_image)
+        loss += content_weight * content_loss(base_image_features, combination_features)
 
-    # get the gradients of the generated image wrt the loss
-    grads = K.gradients(loss, combination_image)
+        feature_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+        for layer_name in feature_layers:
+            layer_features = outputs_dict[layer_name]
+            style_reference_features = layer_features[1, :, :, :]
+            combination_features = layer_features[2, :, :, :]
+            sl = style_loss(style_reference_features, combination_features)
+            loss += (style_weight / len(feature_layers)) * sl
+        loss += total_variation_weight * total_variation_loss(combination_image)
 
-    outputs = [loss]
-    if isinstance(grads, (list, tuple)):
-        outputs += grads
-    else:
-        outputs.append(grads)
+        # get the gradients of the generated image wrt the loss
+        grads = K.gradients(loss, combination_image)
 
-    f_outputs = K.function([combination_image], outputs)
-
-
-    def eval_loss_and_grads(x):
-        if K.image_data_format() == 'channels_first':
-            x = x.reshape((1, 3, img_nrows, img_ncols))
+        outputs = [loss]
+        if isinstance(grads, (list, tuple)):
+            outputs += grads
         else:
-            x = x.reshape((1, img_nrows, img_ncols, 3))
-        outs = f_outputs([x])
-        loss_value = outs[0]
-        if len(outs[1:]) == 1:
-            grad_values = outs[1].flatten().astype('float64')
-        else:
-            grad_values = np.array(outs[1:]).flatten().astype('float64')
-        return loss_value, grad_values
+            outputs.append(grads)
 
-    # this Evaluator class makes it possible
-    # to compute loss and gradients in one pass
-    # while retrieving them via two separate functions,
-    # "loss" and "grads". This is done because scipy.optimize
-    # requires separate functions for loss and gradients,
-    # but computing them separately would be inefficient.
+        f_outputs = K.function([combination_image], outputs)
 
 
-    class Evaluator(object):
+        def eval_loss_and_grads(x):
+            if K.image_data_format() == 'channels_first':
+                x = x.reshape((1, 3, img_nrows, img_ncols))
+            else:
+                x = x.reshape((1, img_nrows, img_ncols, 3))
+            outs = f_outputs([x])
+            loss_value = outs[0]
+            if len(outs[1:]) == 1:
+                grad_values = outs[1].flatten().astype('float64')
+            else:
+                grad_values = np.array(outs[1:]).flatten().astype('float64')
+            return loss_value, grad_values
 
-        def __init__(self):
-            self.loss_value = None
-            self.grads_values = None
+        # this Evaluator class makes it possible
+        # to compute loss and gradients in one pass
+        # while retrieving them via two separate functions,
+        # "loss" and "grads". This is done because scipy.optimize
+        # requires separate functions for loss and gradients,
+        # but computing them separately would be inefficient.
 
-        def loss(self, x):
-            assert self.loss_value is None
-            loss_value, grad_values = eval_loss_and_grads(x)
-            self.loss_value = loss_value
-            self.grad_values = grad_values
-            return self.loss_value
 
-        def grads(self, x):
-            assert self.loss_value is not None
-            grad_values = np.copy(self.grad_values)
-            self.loss_value = None
-            self.grad_values = None
-            return grad_values
+        class Evaluator(object):
 
-    evaluator = Evaluator()
+            def __init__(self):
+                self.loss_value = None
+                self.grads_values = None
 
-    # run scipy-based optimization (L-BFGS) over the pixels of the generated image
-    # so as to minimize the neural style loss
-    x = preprocess_image(base_image_path)
+            def loss(self, x):
+                assert self.loss_value is None
+                loss_value, grad_values = eval_loss_and_grads(x)
+                self.loss_value = loss_value
+                self.grad_values = grad_values
+                return self.loss_value
 
-    for i in range(iterations):
-        print('Start of iteration', i)
-        start_time = time.time()
-        x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(), fprime=evaluator.grads, maxfun=20)
-        print('Current loss value:', min_val)
-        # save current generated image
-        img = deprocess_image(x.copy())
-        fname = result_prefix + str(img_index) + '.png'
-        save_img(fname, img)
-        end_time = time.time()
-        print('Image saved as', fname)
-        print('Iteration %d completed in %ds' % (i, end_time - start_time))
+            def grads(self, x):
+                assert self.loss_value is not None
+                grad_values = np.copy(self.grad_values)
+                self.loss_value = None
+                self.grad_values = None
+                return grad_values
+
+        evaluator = Evaluator()
+
+        # run scipy-based optimization (L-BFGS) over the pixels of the generated image
+        # so as to minimize the neural style loss
+        x = preprocess_image(base_image_path)
+
+        for i in range(iterations):
+            print('Start of iteration', i)
+            start_time = time.time()
+            x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(), fprime=evaluator.grads, maxfun=20)
+            print('Current loss value:', min_val)
+            # save current generated image
+            img = deprocess_image(x.copy())
+            fname = result_prefix + str(img_index) + '.png'
+            save_img(fname, img)
+            end_time = time.time()
+            print('Image saved as', fname)
+            print('Iteration %d completed in %ds' % (i, end_time - start_time))
 
 main('mnist/channel3_32/x_train/', 'style/halftone_32.png', 'mnist/halftone/x_train/')
