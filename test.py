760572b2403bb69dd8407a27f9ae99fa3e7e2466
wuling31715 <wuling31715@gmail.com>
2018-08-07 15:17:10 +0800

    test

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ while True:
         main('mnist/channel3_32/x_train/', 'style/halftone_32.png', 'mnist/halftone/x_train/')
         print('clear session...')
         K.clear_session()
+        sleep(5)
     except:
         print('wait...')        
         sleep(3)
