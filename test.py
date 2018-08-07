8b2c18bc84b605c11b7716a665dc8e08d60912cf
wuling31715 <wuling31715@gmail.com>
2018-08-07 15:09:18 +0800

    test

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 import time
-import sys, os
-print(os.path.dirname(sys.executable))
+from train.vgg19 import main
+from keras import backend as K
+
+K.clean_session()
+while True:
+    try:
+        main('mnist/channel3_32/x_train/', 'style/halftone_32.png', 'mnist/halftone/x_train/')
+    except:
+        print('wait')
+        time.sleep(3)
