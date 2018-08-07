2e80810be67a362211f91f90e2254d3564e305cc
wuling31715 <wuling31715@gmail.com>
2018-08-07 11:00:59 +0800

    test

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ from keras import backend as K
 
 while True:
     main('mnist/channel3_32/x_train/', 'style/halftone_32.png', 'mnist/halftone/x_train/')    
-    K.clear_session()
+    # K.clear_session()
     time.sleep(5)
     
