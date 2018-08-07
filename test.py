4684eefc919a2b30f4e0cb05ff3f5a79e1e505d2
wuling31715 <wuling31715@gmail.com>
2018-08-07 10:59:55 +0800

    test

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ from keras import backend as K
 while True:
     main('mnist/channel3_32/x_train/', 'style/halftone_32.png', 'mnist/halftone/x_train/')    
     K.clear_session()
-    time.sleep(3)
+    time.sleep(5)
     
--- train/vgg19.py
+++ train/vgg19.py
@@ -87,7 +87,7 @@ def main(base_image_path_dir, style_reference_image_path, result_prefix_dir, ite
 
     max_index = get_max()
             
-    for i in range(max_index, max_index+30):
+    for i in range(max_index, max_index+3):
         img_index = i
         base_image_path = base_image_path_dir + str(img_index) + '.png'
 
