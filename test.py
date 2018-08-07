053e6960538bc6348761aa94f6e7aa0767b69510
wuling31715 <wuling31715@gmail.com>
2018-08-07 15:10:34 +0800

    test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import time
 from train.vgg19 import main
 from keras import backend as K
 
-K.clean_session()
+K.clear_session()
 while True:
     try:
         main('mnist/channel3_32/x_train/', 'style/halftone_32.png', 'mnist/halftone/x_train/')
