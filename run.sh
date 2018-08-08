86fbbd53194392ec9e4e4002c0d975c0f75bd44e
wuling31715 <wuling31715@gmail.com>
2018-08-08 09:45:51 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-pkill -9 python
-/home/iis/anaconda3/envs/py3.5/bin/python /home/iis/wuling31715/captcha_generator/test.py
+#!/bin/sh
+
+while true
+do
+    /home/iis/anaconda3/envs/py3.5/bin/python /home/iis/wuling31715/captcha_generator/test.py
+    pkill -9 python
+    sleep 5
+done
