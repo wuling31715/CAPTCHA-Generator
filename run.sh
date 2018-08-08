a40bab64361d71f37894ededc59048d3b7827459
wuling31715 <wuling31715@gmail.com>
2018-08-08 09:50:06 +0800

    it works!!!!!!!!!!!!!!

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-
 while true
 do
-    /home/iis/anaconda3/envs/py3.5/bin/python /home/iis/wuling31715/captcha_generator/test.py
     pkill -9 python
-    sleep 5
+    /home/iis/anaconda3/envs/py3.5/bin/python /home/iis/wuling31715/captcha_generator/test.py    
 done
