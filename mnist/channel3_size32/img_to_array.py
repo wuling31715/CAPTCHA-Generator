59c52f2982c24f28d839161d71a7628496304e7c
wuling31715 <wuling31715@gmail.com>
2018-08-10 14:09:46 +0800

    ignore

--- mnist/channel3_size32/img_to_array.py
+++ mnist/channel3_size32/img_to_array.py
@@ -7,8 +7,8 @@ def img_load(path):
     return img
 
 img_list = []
-for i in range(60000):
-    path = 'x_train/%s.png' % str(i)
+for i in range(10000):
+    path = 'x_test/%s.png' % str(i)
     img = img_load(path)
     img_list.append(img)
     print(path)
@@ -16,8 +16,8 @@ for i in range(60000):
 img_array = np.asarray(img_list)
 print(img_array.shape)
 
-np.save('x_train.npy', img_array)
-test = np.load('x_train.npy')
+np.save('x_test.npy', img_array)
+test = np.load('x_test.npy')
 print(test.shape)
 
 if img_array.shape == test.shape:
