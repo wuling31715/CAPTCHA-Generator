7964f892babb82d6c52b619dfbe95463e19dff8d
wuling31715 <wuling31715@gmail.com>
2018-08-16 10:05:19 +0800

    20180816

--- mnist/halftone_size32/networks/lenet.py
+++ mnist/halftone_size32/networks/lenet.py
@@ -7,12 +7,20 @@ class LeNet:
         model = Sequential()
         #Layer 1
         #Conv Layer 1
-        model.add(Conv2D(input_shape = (32, 32, 3), filters = 6, kernel_size = 5, strides = 1, activation = 'relu'))
+        model.add(Conv2D(filters = 6, 
+                        kernel_size = 5, 
+                        strides = 1, 
+                        activation = 'relu', 
+                        input_shape = (32, 32, 3)))
         #Pooling layer 1
         model.add(MaxPooling2D(pool_size = 2, strides = 2))
         #Layer 2
         #Conv Layer 2
-        model.add(Conv2D(input_shape = (14, 14, 6), filters = 16,  kernel_size = 5, strides = 1, activation = 'relu'))
+        model.add(Conv2D(filters = 16, 
+                        kernel_size = 5,
+                        strides = 1,
+                        activation = 'relu',
+                        input_shape = (14, 14, 6)))
         #Pooling Layer 2
         model.add(MaxPooling2D(pool_size = 2, strides = 2))
         #Flatten
